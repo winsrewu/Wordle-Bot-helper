420a2648173101ad40facaa7acebbb1cb50a38e6
WisW <78122384+winsrewu@users.noreply.github.com>
2024-07-28 00:05:08 +0800

    feat: make this a wordle helper
    1. Readme update
    2. Make generate_pattern_dict() 3 times faster
    3. Make it a real wordle helper, not just a test bot. Now you can use this to solve Wordles.
    4. Add .gitignore

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Wordle-Bot
-An entropy-based strategy to wordle
+An entropy-based strategy to wordle  
+A wordle game helper
 
 
 Inspired by [3Blue1Brown](https://www.youtube.com/watch?v=v68zYyaEmEA)
@@ -16,16 +17,113 @@ git clone https://github.com/GillesVandewiele/Wordle-Bot.git
 (python -m) pip install tqdm scipy
 ```
 
-3. Run the script (it will generate 10 random words and apply the strategy)
+3. Run the script  
+**Needs A LOT OF MEMORY (30 GB i think)**
 ```bash
 python wordle.py
 ```
 
+## Tutorial
+The program will show you top 10 choices.
+```
+*---*
+tares 4.293390057374009
+lares 4.262798837004158
+rales 4.238139680362863
+rates 4.225593399668317
+teras 4.211991450065488
+nares 4.205206640216656
+soare 4.201439131142605
+tales 4.196997695140222
+reais 4.193386309251172
+tears 4.181298541459465
+*---*
+```
+
+You can pick any available word.  
+"00200" means gary, gray, green, gray, gray
+```
+*---*
+tares 4.293390057374009
+lares 4.262798837004158
+rales 4.238139680362863
+rates 4.225593399668317
+teras 4.211991450065488
+nares 4.205206640216656
+soare 4.201439131142605
+tales 4.196997695140222
+reais 4.193386309251172
+tears 4.181298541459465
+*---*
+>>> tares 00200
+```
+0 -> gray  
+1 -> yello  
+2 -> green  
+
+Type exit to exit.
+```
+>>> exit
+...\Wordle-Bot-helper>
+```
+
 ## Example
 
-![Interactive](wordle_interactive.png)
-
-![Example of execution](wordle_bot.png)
+```cmd
+Loaded dictionary with 12972 words...
+Calculating entropies...
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████| 12972/12972 [00:39<00:00, 324.68it/s]
+*---*
+tares 4.293390057374009
+lares 4.262798837004158
+rales 4.238139680362863
+rates 4.225593399668317
+teras 4.211991450065488
+nares 4.205206640216656
+soare 4.201439131142605
+tales 4.196997695140222
+reais 4.193386309251172
+tears 4.181298541459465
+*---*
+>>> guide
+Bad input
+>>> guide 00010
+Calculating entropies...
+100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████| 272/272 [00:00<00:00, 2035.11it/s]
+*---*
+darns 3.8522811547057554
+dryas 3.7752074369378987
+donas 3.747710222337526
+dorsa 3.736476739159818
+doors 3.7168658751806554
+drays 3.7139417969559454
+drows 3.689944977119012
+drats 3.688956827689966
+draws 3.6888457392697096
+drony 3.6861005488945997
+*---*
+>>> darns 10100
+Calculating entropies...
+100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 8/8 [00:00<?, ?it/s] 
+*---*
+hoord 1.9061547465398494
+loord 1.7328679513998633
+boord 1.732867951399863
+brood 1.732867951399863
+chord 1.6674619334292946
+hydro 1.4941751382893083
+crowd 1.3862943611198904
+fjord 1.2130075659799042
+*---*
+>>> hoord 10222
+Calculating entropies...
+100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<?, ?it/s] 
+*---*
+>>> chord 0.0
+*---*
+exit
+```
+![Website](wordle_website.png)
 
 ## Statistics
 
--- wordle.py
+++ wordle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
+from functools import partial
+import math
 import os
 import itertools
 import random
 import pickle
+import multiprocessing
 from tqdm import tqdm
 from scipy.stats import entropy
 from collections import defaultdict, Counter
 
 N_GUESSES = 10
-DICT_FILE_all = 'all_words.txt'
-DICT_FILE = 'words.txt'
+DICT_FILE_available = 'all_words.txt'
+DICT_FILE_answer = 'words.txt'
 SAVE_TIME = False
 
 def calculate_pattern(guess, true):
@@ -36,6 +39,18 @@ def calculate_pattern(guess, true):
     return tuple(pattern)
 
 
+def generate_sub_pattern_dict(dictionary_all, dic):
+    pattern_dict = defaultdict(lambda: defaultdict(set))
+    dictionary = dic["d"]
+    id = dic["i"]
+
+    for word in tqdm(dictionary, desc=f"worker {id}"):
+        for word2 in dictionary_all:
+            pattern = calculate_pattern(word, word2)
+            pattern_dict[word][pattern].add(word2)
+
+    return dict(pattern_dict)
+
 def generate_pattern_dict(dictionary):
     """For each word and possible information returned, store a list
     of candidate words
@@ -45,19 +60,43 @@ def generate_pattern_dict(dictionary):
     >>> sorted(pattern_dict['crane'][(0, 1, 2, 0, 1)])
     ['bears', 'weary']
     """
+    print("Generating pattern dict...")
     pattern_dict = defaultdict(lambda: defaultdict(set))
-    for word in tqdm(dictionary):
-        for word2 in dictionary:
-            pattern = calculate_pattern(word, word2)
-            pattern_dict[word][pattern].add(word2)
+    pool = multiprocessing.Pool()
+    process_num = multiprocessing.cpu_count()
+    g_func = partial(generate_sub_pattern_dict, dictionary)
+    sub_l = []
+
+    for i in range(process_num):
+        l = math.floor(i / process_num * len(dictionary))
+        r = math.floor((i + 1) / process_num * len(dictionary))
+        sub_l.append({
+            "d": dictionary[l : r],
+            "i": i
+        })
+
+    results = pool.map(g_func, sub_l)
+    pool.close()
+    pool.join()
+
+    for i in results:
+        pattern_dict.update(i)
+
     return dict(pattern_dict)
+
+    # for word in tqdm(dictionary):
+    #     for word2 in dictionary:
+    #         pattern = calculate_pattern(word, word2)
+    #         pattern_dict[word][pattern].add(word2)
+    # return dict(pattern_dict)
 
 
 def calculate_entropies(words, possible_words, pattern_dict, all_patterns):
     """Calculate the entropy for every word in `words`, taking into account
     the remaining `possible_words`"""
+    print("Calculating entropies...")
     entropies = {}
-    for word in words:
+    for word in tqdm(words):
         counts = []
         for pattern in all_patterns:
             matches = pattern_dict[word][pattern]
@@ -66,14 +105,20 @@ def calculate_entropies(words, possible_words, pattern_dict, all_patterns):
         entropies[word] = entropy(counts)
     return entropies
 
+def print_cdds(cdds):
+    print("*---*")
+    for cdd in cdds:
+        print(cdd[0] + " " + str(cdd[1]))
+    print("*---*")
 
 def main():
+    ## init
     # load all 5-letter-words for making patterns 
-    with open(DICT_FILE_all) as ifp:
+    with open(DICT_FILE_available) as ifp:
         all_dictionary = list(map(lambda x: x.strip(), ifp.readlines()))
 
     # Load 2315 words for solutions
-    with open(DICT_FILE) as ifp:
+    with open(DICT_FILE_answer) as ifp:
         dictionary = list(map(lambda x: x.strip(), ifp.readlines()))
 
     error_msg = 'Dictionary contains different length words.'
@@ -91,45 +136,51 @@ def main():
         pattern_dict = generate_pattern_dict(all_dictionary)
         pickle.dump(pattern_dict, open('pattern_dict.p', 'wb+'))
 
-    # Simulate games
-    stats = defaultdict(list)
+    ##--##
 
-    for WORD_TO_GUESS in tqdm(dictionary):
+    ## main loop
 
-        if SAVE_TIME:
-            guess_word = 'tares'
-            all_words = set(all_dictionary)
-            info = calculate_pattern(guess_word, WORD_TO_GUESS)
-            words = pattern_dict[guess_word][info]
-            all_words = all_words.intersection(words)
-            init_round = 1
-        else:
-            all_words = set(all_dictionary)
-            init_round = 0
-
-        for n_round in range(init_round, N_GUESSES):
-
-            candidates = all_dictionary
-            entropies = calculate_entropies(candidates, all_words, pattern_dict, all_patterns)
-
-            if max(entropies.values()) < 0.1:
-                candidates = all_words
-                entropies = calculate_entropies(candidates, all_words, pattern_dict, all_patterns)
-
-            # Guess the candiate with highest entropy
-            guess_word = max(entropies.items(), key=lambda x: x[1])[0]
-            info = calculate_pattern(guess_word, WORD_TO_GUESS)
-
-            # Print round information
-            print('Guessing:     ', guess_word)
-            print('Info:         ', info)
-            if guess_word == WORD_TO_GUESS:
-                print(f'WIN IN {n_round + 1} GUESSES!\n\n\n')
+    init_flag = False
+    input_c = ""
+    available_words = set()
+    candidates = []
+    while True:
+        if init_flag:
+            input_c = input()
+            if input_c == "exit":
                 break
 
-            # Filter our list of remaining possible words
-            words = pattern_dict[guess_word][info]
-            all_words = all_words.intersection(words)
+            input_sp = input_c.split()
+            if (len(input_sp) != 2) or (not input_sp[0] in available_words) or (len(input_sp[1]) != 5):
+                print("Bad input")
+                continue
+
+            p_l = []
+            bad_flag = False
+            for c in input_sp[1]:
+                if int(c) < 0 or int(c) > 2:
+                    print("Bad input")
+                    bad_flag = True
+                    break
+                p_l.append(int(c))
+            if bad_flag:
+                continue
+                
+            pattern = tuple(p_l)
+            available_words = available_words.intersection(pattern_dict[input_sp[0]][pattern])
+            if len(available_words) < 100:
+                candidates = available_words.intersection(dictionary)
+            else:
+                candidates = available_words
+            candidates = available_words
+        else:
+            init_flag = True
+            available_words = set(all_dictionary)
+            candidates = all_dictionary
+
+        entropies = calculate_entropies(candidates, available_words, pattern_dict, all_patterns)
+        top_10_cdds = sorted(entropies.items(), key=lambda x: x[1], reverse=True)[:10]
+        print_cdds(top_10_cdds)
 
 if __name__ == "__main__":
     main()
